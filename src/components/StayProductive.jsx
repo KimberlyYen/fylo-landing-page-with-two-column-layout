711ce97c0c5f9164275b19c88f3017b0685846e0
kimberly yen <kim223305@gmail.com>
2022-08-18 16:19:52 +0800

    Add card components

--- src/components/StayProductive.jsx
+++ src/components/StayProductive.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import illustration2 from "../public/illustration-2.svg";
 import arrow from "../public/icon-arrow.svg";
+import quotes from "../public/icon-quotes.svg";
+import avatarTestimonial from "../public/avatar-testimonial.jpg";
 
 
 
@@ -15,13 +17,36 @@ export default function StayProductive() {
                 <p>Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required! </p>
             </div>
 
-            <div className="flex items-center justify-center w-1/2 pb-1 mx-auto mt-8 border-b row border-b-Moderate-Cyan ">
+            <div className="flex items-center justify-center w-1/3 pb-1 mx-auto mt-8 border-b row border-b-Moderate-Cyan ">
                 <p className="pr-2 text-xs font-light text-Moderate-Cyan ">See how Fylo works</p>
                 <img src={arrow} alt="arrow" />
             </div>
+
+        <Card/>
             
             
         </div>
     )
     
+}
+
+function Card() {
+    return (<>
+        <div className="mx-2 my-10 bg-white shadow-md rounded-xl shadow-gray-300-400">
+            <p className="px-8 py-2 text-sm leading-loose text-left text-Desaturated-Blue">
+            <img className="my-4" src={quotes} alt="quotes" />
+                Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.
+            
+            <div className="flex place-items-center">
+                <img className="w-10 my-4 rounded-full" src={avatarTestimonial} alt="avatar-testimonial" />
+                <div className="pl-4 leading-normal">
+                    <h3 className="font-bold text-md text-very-dark-Blue">Kyle Burton</h3>
+                    <p className="text-[10px]"> Founder & CEO, Huddle</p>
+                </div>
+            </div>
+            </p>
+
+        </div>
+    </>    
+    )
 }
